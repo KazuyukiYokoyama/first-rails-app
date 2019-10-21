beff0f870e607ab97fc36ee475ee75be61d176b4
kazuyukiyokoyama <dime72.elephant@gmail.com>
2019-10-21 13:08:46 +0900

    Boardオブジェクトの基本的なCRUD機能を実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,8 +8,31 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
-    binding.pry
+    board = Board.create(board_params)
+
+    redirect_to board
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+    
+    redirect_to board
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+
+    redirect_to boards_path
   end
 
   private
