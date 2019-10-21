9b8df3fa78cf7129da22d56b8fcd2e3bf822d21d
kazuyukiyokoyama <dime72.elephant@gmail.com>
2019-10-21 16:20:43 +0900

    コメント機能を実装するため、Commentオブジェクトを作成

---
+++ app/models/comment.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  board_id   :integer
+#  name       :string(255)      not null
+#  comment    :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_comments_on_board_id  (board_id)
+#
+
+class Comment < ApplicationRecord
+  belongs_to :board
+end
